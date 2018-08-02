31c5d6a9ffc9bfebb9faf265dddf0b43c78b90b2
liangziqiang <2416004895@qq.com>
2018-08-02 10:57:39 +0800

    实现Spliterator接口的demo

---
+++ src/main/java/zym/stream/streamfresh/TaggedArrayTest.java
@@ -0,0 +1,18 @@
+package zym.stream.streamfresh;
+
+import org.junit.Test;
+
+/**
+ * @Author unyielding
+ * @date 2018/7/31 0031 22:46
+ * @desc 测试 TaggedArray类
+ */
+public class TaggedArrayTest {
+    @Test
+    public void  parEach() {
+        TaggedArray<String> taggedArray = new TaggedArray<>(new
+                String[]{"我爱你", "你爱我"}, new String[]{"1", "2"});
+
+        TaggedArray.parEach(taggedArray, System.out::println);
+    }
+}
