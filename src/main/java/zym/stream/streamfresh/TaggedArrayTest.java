69ec9a4eb85b352e2d7aa7948834dea2f43b18c8
liangziqiang <2416004895@qq.com>
2018-08-03 10:12:59 +0800

    小幅调整

--- src/main/java/zym/stream/streamfresh/TaggedArrayTest.java
+++ src/main/java/zym/stream/streamfresh/TaggedArrayTest.java
@@ -1,9 +1,17 @@
 package zym.stream.streamfresh;
 
+import org.junit.Test;
+
 /**
  * @Author unyielding
  * @date 2018/7/31 0031 22:46
- * @desc
+ * @desc 测试 TaggedArray类
  */
 public class TaggedArrayTest {
+    @Test
+    public void  parEach() {
+        TaggedArray<String> taggedArray = new TaggedArray<>(new
+                String[]{"我爱你", "你爱我"}, new String[]{"1", "2"});
+        TaggedArray.parEach(taggedArray, System.out::println);
+    }
 }
