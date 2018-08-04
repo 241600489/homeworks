ae11390c917633bab891e777b5130d5c4a15c5ee
liangziqiang <2416004895@qq.com>
2018-08-04 10:36:54 +0800

    bean

---
+++ src/main/java/zym/stream/datatransfer/Record.java
@@ -0,0 +1,373 @@
+package zym.stream.datatransfer;
+
+public class Record {
+    private Long id;
+
+    private Long recordid;
+
+    private String srcsbbh;
+
+    private String sbbh;
+
+    private String wfsbbh;
+
+    private String zqmj;
+
+    private String srcclfl;
+
+    private String clfl;
+
+    private String wfclfl;
+
+    private String hphm;
+
+    private String srchpzl;
+
+    private String hpzl;
+
+    private String wfhpzl;
+
+    private String xzqh;
+
+    private String wfdd;
+
+    private String lddm;
+
+    private String ddms;
+
+    private String wfdz;
+
+    private Long wfsj;
+
+    private String srcwfxw;
+
+    private String wfxw;
+
+    private String wfwfxw;
+
+    private Integer scz;
+
+    private Integer bzz;
+
+    private String zpwjm;
+
+    private String zpstr1;
+
+    private String zppath1;
+
+    private String zpstr2;
+
+    private String zppath2;
+
+    private String zpstr3;
+
+    private String zppath3;
+
+    private Boolean srctype;
+
+    private Boolean status;
+
+    private String msg;
+
+    private Long createtime;
+
+    private Long updatetime;
+
+    private Long uploadtime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRecordid() {
+        return recordid;
+    }
+
+    public void setRecordid(Long recordid) {
+        this.recordid = recordid;
+    }
+
+    public String getSrcsbbh() {
+        return srcsbbh;
+    }
+
+    public void setSrcsbbh(String srcsbbh) {
+        this.srcsbbh = srcsbbh == null ? null : srcsbbh.trim();
+    }
+
+    public String getSbbh() {
+        return sbbh;
+    }
+
+    public void setSbbh(String sbbh) {
+        this.sbbh = sbbh == null ? null : sbbh.trim();
+    }
+
+    public String getWfsbbh() {
+        return wfsbbh;
+    }
+
+    public void setWfsbbh(String wfsbbh) {
+        this.wfsbbh = wfsbbh == null ? null : wfsbbh.trim();
+    }
+
+    public String getZqmj() {
+        return zqmj;
+    }
+
+    public void setZqmj(String zqmj) {
+        this.zqmj = zqmj == null ? null : zqmj.trim();
+    }
+
+    public String getSrcclfl() {
+        return srcclfl;
+    }
+
+    public void setSrcclfl(String srcclfl) {
+        this.srcclfl = srcclfl == null ? null : srcclfl.trim();
+    }
+
+    public String getClfl() {
+        return clfl;
+    }
+
+    public void setClfl(String clfl) {
+        this.clfl = clfl == null ? null : clfl.trim();
+    }
+
+    public String getWfclfl() {
+        return wfclfl;
+    }
+
+    public void setWfclfl(String wfclfl) {
+        this.wfclfl = wfclfl == null ? null : wfclfl.trim();
+    }
+
+    public String getHphm() {
+        return hphm;
+    }
+
+    public void setHphm(String hphm) {
+        this.hphm = hphm == null ? null : hphm.trim();
+    }
+
+    public String getSrchpzl() {
+        return srchpzl;
+    }
+
+    public void setSrchpzl(String srchpzl) {
+        this.srchpzl = srchpzl == null ? null : srchpzl.trim();
+    }
+
+    public String getHpzl() {
+        return hpzl;
+    }
+
+    public void setHpzl(String hpzl) {
+        this.hpzl = hpzl == null ? null : hpzl.trim();
+    }
+
+    public String getWfhpzl() {
+        return wfhpzl;
+    }
+
+    public void setWfhpzl(String wfhpzl) {
+        this.wfhpzl = wfhpzl == null ? null : wfhpzl.trim();
+    }
+
+    public String getXzqh() {
+        return xzqh;
+    }
+
+    public void setXzqh(String xzqh) {
+        this.xzqh = xzqh == null ? null : xzqh.trim();
+    }
+
+    public String getWfdd() {
+        return wfdd;
+    }
+
+    public void setWfdd(String wfdd) {
+        this.wfdd = wfdd == null ? null : wfdd.trim();
+    }
+
+    public String getLddm() {
+        return lddm;
+    }
+
+    public void setLddm(String lddm) {
+        this.lddm = lddm == null ? null : lddm.trim();
+    }
+
+    public String getDdms() {
+        return ddms;
+    }
+
+    public void setDdms(String ddms) {
+        this.ddms = ddms == null ? null : ddms.trim();
+    }
+
+    public String getWfdz() {
+        return wfdz;
+    }
+
+    public void setWfdz(String wfdz) {
+        this.wfdz = wfdz == null ? null : wfdz.trim();
+    }
+
+    public Long getWfsj() {
+        return wfsj;
+    }
+
+    public void setWfsj(Long wfsj) {
+        this.wfsj = wfsj;
+    }
+
+    public String getSrcwfxw() {
+        return srcwfxw;
+    }
+
+    public void setSrcwfxw(String srcwfxw) {
+        this.srcwfxw = srcwfxw == null ? null : srcwfxw.trim();
+    }
+
+    public String getWfxw() {
+        return wfxw;
+    }
+
+    public void setWfxw(String wfxw) {
+        this.wfxw = wfxw == null ? null : wfxw.trim();
+    }
+
+    public String getWfwfxw() {
+        return wfwfxw;
+    }
+
+    public void setWfwfxw(String wfwfxw) {
+        this.wfwfxw = wfwfxw == null ? null : wfwfxw.trim();
+    }
+
+    public Integer getScz() {
+        return scz;
+    }
+
+    public void setScz(Integer scz) {
+        this.scz = scz;
+    }
+
+    public Integer getBzz() {
+        return bzz;
+    }
+
+    public void setBzz(Integer bzz) {
+        this.bzz = bzz;
+    }
+
+    public String getZpwjm() {
+        return zpwjm;
+    }
+
+    public void setZpwjm(String zpwjm) {
+        this.zpwjm = zpwjm == null ? null : zpwjm.trim();
+    }
+
+    public String getZpstr1() {
+        return zpstr1;
+    }
+
+    public void setZpstr1(String zpstr1) {
+        this.zpstr1 = zpstr1 == null ? null : zpstr1.trim();
+    }
+
+    public String getZppath1() {
+        return zppath1;
+    }
+
+    public void setZppath1(String zppath1) {
+        this.zppath1 = zppath1 == null ? null : zppath1.trim();
+    }
+
+    public String getZpstr2() {
+        return zpstr2;
+    }
+
+    public void setZpstr2(String zpstr2) {
+        this.zpstr2 = zpstr2 == null ? null : zpstr2.trim();
+    }
+
+    public String getZppath2() {
+        return zppath2;
+    }
+
+    public void setZppath2(String zppath2) {
+        this.zppath2 = zppath2 == null ? null : zppath2.trim();
+    }
+
+    public String getZpstr3() {
+        return zpstr3;
+    }
+
+    public void setZpstr3(String zpstr3) {
+        this.zpstr3 = zpstr3 == null ? null : zpstr3.trim();
+    }
+
+    public String getZppath3() {
+        return zppath3;
+    }
+
+    public void setZppath3(String zppath3) {
+        this.zppath3 = zppath3 == null ? null : zppath3.trim();
+    }
+
+    public Boolean getSrctype() {
+        return srctype;
+    }
+
+    public void setSrctype(Boolean srctype) {
+        this.srctype = srctype;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg == null ? null : msg.trim();
+    }
+
+    public Long getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Long createtime) {
+        this.createtime = createtime;
+    }
+
+    public Long getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Long updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public Long getUploadtime() {
+        return uploadtime;
+    }
+
+    public void setUploadtime(Long uploadtime) {
+        this.uploadtime = uploadtime;
+    }
+}
