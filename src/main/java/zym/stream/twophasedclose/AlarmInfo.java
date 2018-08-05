c977abb86604d47386ad40864fc01cc6b8389616
liangziqiang <2416004895@qq.com>
2018-08-05 20:48:43 +0800

    报警信息 实体类

--- src/main/java/zym/stream/twophasedclose/AlarmInfo.java
+++ src/main/java/zym/stream/twophasedclose/AlarmInfo.java
@@ -3,7 +3,19 @@ package zym.stream.twophasedclose;
 /**
  * @Author unyielding
  * @date 2018/8/5 0005 11:17
- * @desc
+ * @desc 告警信息
  */
 public class AlarmInfo {
+    private AlarmType type;
+    private String id;
+    private String extraInfo;
+
+    public AlarmInfo(AlarmType type, String id) {
+        this.type = type;
+        this.id = id;
+    }
+
+    public void setExtraInfo(String extraInfo) {
+        this.extraInfo = extraInfo;
+    }
 }
