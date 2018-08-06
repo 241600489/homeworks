543fc659496616adf035766374bccd4d4ee0a89c
liangziqiang <2416004895@qq.com>
2018-08-07 07:21:37 +0800

    同步数据模块入口类

---
+++ src/main/java/zym/stream/promise/DataSyncTask.java
@@ -0,0 +1,24 @@
+package zym.stream.promise;
+
+import java.util.Map;
+
+/**
+ * @Author unyielding
+ * @date 2018/8/7 0007 7:10
+ * @desc 数据同步模块的入口类
+ */
+public class DataSyncTask implements Runnable{
+    private final Map<String,String> taskParameters;
+
+    public DataSyncTask(Map<String, String> taskParameters) {
+        this.taskParameters = taskParameters;
+    }
+
+    @Override
+    public void run() {
+        String ftpServer = taskParameters.get("ftpServer");
+        String ftpUserName = taskParameters.get("ftpUserName");
+        String ftpPassword = taskParameters.get("ftpPasswordz");
+        
+    }
+}
