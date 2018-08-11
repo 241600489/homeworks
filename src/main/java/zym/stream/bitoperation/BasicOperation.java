bd10c8e8828d5990a943b4f6694e727300357074
liangziqiang <2416004895@qq.com>
2018-08-11 19:11:06 +0800

    练习位掩码

---
+++ src/main/java/zym/stream/bitoperation/BasicOperation.java
@@ -0,0 +1,47 @@
+package zym.stream.bitoperation;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @Author unyielding
+ * @date 2018/8/11 0011 18:40
+ * @desc 位的基本运算
+ */
+public class BasicOperation {
+    int mask=0;
+    int enabled = 1 << 0;
+    int pressed = 1 << 1;
+    int armed = 1 << 2;
+    int selected = 1 << 3;
+
+    @Before
+    public void before() {
+        setEnabled(false);
+
+    }
+
+    public void setEnabled(boolean state) {
+        if (state == isEnabled()) {
+            return;
+        }
+        if (state) {
+            mask |= enabled;
+            System.out.println(mask);
+        }else {
+            mask &= ~enabled;
+            System.out.println(mask);
+        }
+    }
+
+    private boolean isEnabled() {
+        return (mask & enabled) != 0;
+    }
+
+    @Test
+    public void testSetEnable() {
+        setEnabled(true);
+        setEnabled(false);
+
+    }
+}
