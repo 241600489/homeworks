d2b9be6d1038c236dab0e3a36f85f01a0b19a8d4
2416004895@qq.com <2416004895@qq.com>
2019-10-07 21:23:24 +0800

    feat:exception finally test

---
+++ src/main/java/zym/interview/dayuwuxian/ExceptionFinally.java
@@ -0,0 +1,39 @@
+package zym.interview.dayuwuxian;
+
+import org.junit.Test;
+
+public class ExceptionFinally {
+    public static void main(String[] args) {
+        System.out.println("返回值为" + str(null, "B"));
+        System.out.println("返回值为" + str(null, 6));
+    }
+
+    private static String str(String a, String b) {
+        try {
+            return a + b;
+        } catch (Exception e) {
+            System.out.println("exception");
+        }finally {
+            System.out.println("finally");
+        }
+        return "";
+    }
+    private static Integer str(Integer a, Integer b) {
+        try {
+            return a + b;
+        } catch (Exception e) {
+            System.out.println("int throws exception");
+        }finally {
+            System.out.println("int finally");
+        }
+        return 0;
+    }
+
+    @Test
+    public void testJiaJia() {
+        int i = 3;
+        i *= ++i;
+        System.out.println(i);
+
+    }
+}
