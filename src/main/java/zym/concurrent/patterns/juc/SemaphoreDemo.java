b45d8713764fb4565a46ae27c3be2c6d32cbd3fa
2416004895@qq.com <2416004895@qq.com>
2019-10-12 23:23:29 +0800

    fix:remove useless code chunk

--- src/main/java/zym/concurrent/patterns/juc/SemaphoreDemo.java
+++ src/main/java/zym/concurrent/patterns/juc/SemaphoreDemo.java
@@ -10,11 +10,9 @@ public class SemaphoreDemo {
 
     public static final int THREAD_SIZE = 10;
 
-    public static void  runSomething() throws InterruptedException {
-        //模拟处理什么事
-        Thread.sleep(1000);
-        System.out.println(String.format("current threadId is %d,current time is %d",
-                Thread.currentThread().getId(), System.currentTimeMillis() / 1000));
+    public static void  runSomething() {
+        System.out.println(String.format("%s,current time is %d",
+                Thread.currentThread().getName(), System.currentTimeMillis() / 1000));
     }
 
     public static void main(String[] args) throws InterruptedException {
