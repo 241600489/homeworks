a98fb6ea21c7d23d06d955d1b4240ef978af869a
2416004895@qq.com <2416004895@qq.com>
2019-10-14 23:03:50 +0800

    fix:增加新的测试用例

--- src/main/java/zym/concurrent/patterns/juc/RateLimiterOnSemaphoreTest.java
+++ src/main/java/zym/concurrent/patterns/juc/RateLimiterOnSemaphoreTest.java
@@ -1,6 +1,7 @@
 package zym.concurrent.patterns.juc;
 
-import org.junit.Test;
+import com.google.common.util.concurrent.RateLimiter;
+import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,12 +19,42 @@ public class RateLimiterOnSemaphoreTest {
             });
         }
     }
-
     private static void doSomething() {
         System.out.println("now is " + System.currentTimeMillis() / 1000);
     }
 
+    @Test
+    public void testRetry() {
+        RateLimiterOnSemaphore rateLimiter = RateLimiterOnSemaphore.create(10);
+        //正式项目慎用
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 20; i++) {
+            executorService.execute(() -> {
+                if (rateLimiter.tryAcquire()) {
+                    doSomething();
+                }else {
+                    System.out.println("失败");
+                }
+            });
+        }
+    }
 
+    @Test
+    public void testGoogleRateLimiterTry() throws InterruptedException {
+        RateLimiter rateLimiter = RateLimiter.create(10);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        Thread.sleep(1000);
+        for (int i = 0; i < 20; i++) {
+            executorService.execute(() -> {
+                if (rateLimiter.tryAcquire()) {
+                    doSomething();
+                }else {
+                    System.out.println("失败");
+                }
+            });
+        }
+
+    }
 
 
 }
